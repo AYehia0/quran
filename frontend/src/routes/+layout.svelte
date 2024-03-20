0f087a06340bbe0ec21ed0ee17359f3a9a61c017
AYehia0 <ahmedyehia611@gmail.com>
2024-03-20 18:04:35 +0200

    main layout improvements: removed unused menu and added settings section in the drawer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -37,15 +37,6 @@
 
             <svelte:fragment slot="trail">
                 <div class="flex items-center">
-                    <button class="lg:hidden btn btn-sm mr-4">
-                        <span>
-                            <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-                                <rect width="100" height="20" />
-                                <rect y="30" width="100" height="20" />
-                                <rect y="60" width="100" height="20" />
-                            </svg>
-                        </span>
-                    </button>
                     <button class="btn btn-sm ml-4" on:click={toggleDrawer}>
                         <span>
                             <svg viewBox="0 0 100 80" class="fill-token
@@ -63,6 +54,8 @@
 
 	<svelte:fragment slot="sidebarRight">
         <Drawer>
+			<h2 class="p-4">Settings</h2>
+			<hr class="mb-4" />
             <Navigation/>
         </Drawer>
     </svelte:fragment>
