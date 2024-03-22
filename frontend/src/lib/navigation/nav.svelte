52f6996d9bcecdbd8363440c7365b970a73cc3fb
AYehia0 <ahmedyehia611@gmail.com>
2024-03-22 02:30:38 +0200

    close drawer when select something

--- frontend/src/lib/navigation/nav.svelte
+++ frontend/src/lib/navigation/nav.svelte
@@ -1,6 +1,16 @@
+<script>
+    // close the drawer when a link is clicked
+    import { getDrawerStore } from "@skeletonlabs/skeleton";
+    const drawerStore = getDrawerStore();
+
+    function closeDrawer() {
+        drawerStore.close();
+    }
+</script>
+
 <nav class="list-nav">
 	<ul>
-		<li><a href="/">Homepage</a></li>
-		<li><a href="/bookmarks">Bookmarks</a></li>
+		<li><a href="/" on:click={closeDrawer}>Homepage</a></li>
+		<li><a href="/bookmarks" on:click={closeDrawer}>Bookmarks</a></li>
 	</ul>
 </nav>
